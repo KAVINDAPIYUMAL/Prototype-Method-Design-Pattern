e013597d36aa675d9a157a9b92589448fb13d8b0
INTRANET\015753 <kavinda@slt.com.lk>
2024-10-04 16:59:21 +0530

    initial commit

---
+++ Prototype-Method-Design-Pattern/Prototype-Method-Design-Pattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_Method_Design_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IGraphicElement originalLogo = new Logo("OriginalLogo", "Blue");
+
+            // Clone the original logo to create a new customized logo
+            IGraphicElement customizedLogo = originalLogo.Clone();
+
+            // Display the original and customized logos
+            Console.WriteLine("Original Logo:");
+            originalLogo.Display();
+
+            Console.WriteLine("\nCustomized Logo:");
+            customizedLogo.Display();
+        }
+    }
+}
